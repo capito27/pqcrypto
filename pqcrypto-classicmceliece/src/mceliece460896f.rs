68eec0bb862d36354fa0c49756e7443a8c33654d
Filipe Fortunato <f.filipe.30.03@gmail.com>
2022-06-28 13:20:15 +0200

    Add support for classicmceliece encap with raw mem, instead of struct

--- pqcrypto-classicmceliece/src/mceliece460896f.rs
+++ pqcrypto-classicmceliece/src/mceliece460896f.rs
@@ -141,7 +141,7 @@ macro_rules! encap {
         let mut ss = SharedSecret::new();
         let mut ct = Ciphertext::new();
         assert_eq!(
-            unsafe { ffi::$variant(ct.0.as_mut_ptr(), ss.0.as_mut_ptr(), $pk.0.as_ptr()) },
+            unsafe { ffi::$variant(ct.0.as_mut_ptr(), ss.0.as_mut_ptr(), $pk) },
             0,
         );
         (ss, ct)
@@ -150,6 +150,18 @@ macro_rules! encap {
 
 /// Encapsulate to a mceliece460896f public key
 pub fn encapsulate(pk: &PublicKey) -> (SharedSecret, Ciphertext) {
+    #[cfg(all(enable_x86_avx2, feature = "avx2"))]
+    {
+        if std::is_x86_feature_detected!("avx2") {
+            return encap!(PQCLEAN_MCELIECE460896F_AVX_crypto_kem_enc, pk.0.as_ptr());
+        }
+    }
+    encap!(PQCLEAN_MCELIECE460896F_VEC_crypto_kem_enc, pk.0.as_ptr())
+}
+
+
+/// Encapsulate to a mceliece460896f public key
+pub fn encapsulate_raw(pk: *const u8) -> (SharedSecret, Ciphertext) {
     #[cfg(all(enable_x86_avx2, feature = "avx2"))]
     {
         if std::is_x86_feature_detected!("avx2") {
