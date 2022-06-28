a9a796391da36eebf8c6b16797f3a9f95c842450
Filipe Fortunato <f.filipe.30.03@gmail.com>
2022-06-28 13:40:52 +0200

    fix typo

--- pqcrypto-classicmceliece/src/mceliece460896f.rs
+++ pqcrypto-classicmceliece/src/mceliece460896f.rs
@@ -154,7 +154,7 @@ pub fn encapsulate(pk: &PublicKey) -> (SharedSecret, Ciphertext) {
     #[cfg(all(enable_x86_avx2, feature = "avx2"))]
     {
         if std::is_x86_feature_detected!("avx2") {
-            return encap!(PQCLEAN_MCELIECE460896F_AVX_crypto_kem_enc, k));
+            return encap!(PQCLEAN_MCELIECE460896F_AVX_crypto_kem_enc, k);
         }
     }
     encap!(PQCLEAN_MCELIECE460896F_VEC_crypto_kem_enc, k)
